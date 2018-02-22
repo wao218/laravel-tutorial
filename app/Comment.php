708bb6e833d703683f687a1e79c9f02776e59432
Wesley Osborne <wao218@lehigh.edu>
2018-02-22 10:41:38 -0500

    Elequent Relationships and Comments

---
+++ app/Comment.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+
+class Comment extends Model
+{
+    // $comment->post;
+
+    public function post() {
+
+        return $this->belongsTo(Post::class);
+
+    }
+
+}
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,6 +8,31 @@
 
         {{ $post->body }}
 
+
+        <div class="comments">
+
+            <ul class="list-group">
+
+                @foreach ($post->comments as $comment)
+                
+                <li class="list-group-item">
+
+                    <strong>
+
+                        {{ $comment->created_at->diffForHumans() }}: &nbsp;
+
+                    </strong>
+
+                    {{ $comment->body }}
+
+                </li>
+                
+                @endforeach
+
+            </ul>
+
+        </div>
+
     </div>
 
 @endsection
