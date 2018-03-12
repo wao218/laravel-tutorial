daa63c357bdf3bedd059042d3b399862902f02f9
Wesley Osborne <wao218@lehigh.edu>
2018-03-11 21:13:20 -0400

    Archives

--- app/Post.php
+++ app/Post.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use Carbon\Carbon;
+
 class Post extends Model
 {
     
@@ -28,6 +30,18 @@ class Post extends Model
 
         $this->comments()->create(compact('body'));
         
+    }
+
+    public function scopeFilter($query, $filters) {
+
+        if ($month = $filters['month']) {
+            $query->whereMonth('created_at', Carbon::parse($month)->month);
+        }
+
+        if ($year = $filters['year']) {
+            $query->whereYear('created_at', $year);
+        }
+
     }
 
 }
