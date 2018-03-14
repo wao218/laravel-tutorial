a208c7630611b7f812ef8ca846dd868dc8d4a349
Wesley Osborne <wao218@lehigh.edu>
2018-03-13 22:31:06 -0400

    Form Requests and Form Objects

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -2,54 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\User;
-
-use App\Mail\Welcome;
+use App\Http\Requests\RegistrationForm;
 
 //use Illuminate\Support\Facades\Mail;
 
 class RegistrationController extends Controller
 {
     //
-    public function create() {
+    public function create()
+    {
 
         return view('registration.create');
 
     }
 
-    public function store() {
+    public function store(RegistrationForm $form)
+    {
 
-       // validate the form
+        $form->persist();
        
-        $this->validate(request(), [
-
-            'name' => 'required',
-
-            'email' => 'required|email',
-
-            'password' => 'required|confirmed'
-
-        ]);
-
-       // Create and Save the User
-
-       $user = User::create([
-
-        'name' => request('name'),
-        'email' => request('email'),
-        'password' => bcrypt(request('password'))
-
-       ]);
-
-       // Sign them in
-
-       auth()->login($user);
-
-        // Send User Welcome email
-        \Mail::to($user)->send(new Welcome ($user));
 
        // Redirect to the Home Page
 
-       return redirect()->home();
+        return redirect()->home();
     }
 }
---
+++ app/Http/Requests/RegistrationForm.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\User;
+
+use App\Mail\Welcome;
+
+use Illuminate\Support\Facades\Mail;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegistrationForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|confirmed'
+
+        ];
+    }
+
+    public function persist()
+    {
+        
+        // Create and Save the User
+
+        $user = User::create([
+            'name' => request('name'),
+            'email' => request('email'),
+            'password' => bcrypt(request('password'))
+        ]);
+    
+        // Sign them in
+
+        auth()->login($user);
+
+        // Send User Welcome email
+        \Mail::to($user)->send(new Welcome($user));
+    }
+}
